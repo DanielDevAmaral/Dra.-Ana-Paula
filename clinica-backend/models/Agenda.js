ed3fbce1da4da9694a51da1c0dbbf54cdc7308c9
DanielDevAmaral <daniel.amaral1928@outlook.com>
2024-10-24 21:36:04 -0300

    deleting env

--- clinica-backend/models/Agenda.js
+++ clinica-backend/models/Agenda.js
@@ -1,38 +1,19 @@
 const mongoose = require('mongoose');
 
-// Definindo o schema para o calendário de consultas (Agenda)
 const agendaSchema = new mongoose.Schema({
-    dataInicioConsulta: { 
-        type: Date, 
-        required: true 
-    }, // Data de início da consulta
-    horaInicioConsulta: { 
-        type: String, 
-        required: true 
-    }, // Hora de início da consulta no formato HH:MM
-    dataFimConsulta: { 
-        type: Date, 
-        required: true 
-    }, // Data de fim da consulta
-    horaFimConsulta: { 
-        type: String, 
-        required: true 
-    }, // Hora de fim da consulta no formato HH:MM
-    paciente: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Paciente',
-        required: false // A consulta pode ou não estar vinculada a um paciente
-    }, // Vínculo com o paciente
-    foiPaga: { 
-        type: Boolean, 
-        default: false, 
-        required: true 
-    } // Verifica se a consulta foi paga ou não
+  data: { type: Date, required: true },
+  horarioInicio: { type: String, required: true },
+  horarioFim: { type: String, required: true },
+  paciente: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Paciente', 
+    required: true 
+  }, // Vinculação com um paciente já existente
+  comentarios: { type: String, required: false },
 }, {
-    timestamps: true
+  timestamps: true,
 });
 
-// Criando o modelo Agenda
 const Agenda = mongoose.model('Agenda', agendaSchema);
 
 module.exports = Agenda;
